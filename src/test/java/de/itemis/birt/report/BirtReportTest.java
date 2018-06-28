75b3b2045e103ea737452b2f0b2a14a572989f73
Sascha Rahlff <srRahlff@itemis.com>
2018-06-28 10:08:01 +0200

    Init project Spring Boot Birt Reports.

---
+++ src/test/java/de/itemis/birt/report/BirtReportTest.java
@@ -0,0 +1,17 @@
+package de.itemis.birt.report;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.itemis.birt.Application;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { Application.class})
+public class BirtReportTest {
+	@Test
+	public void reportTest() {
+		
+	}
+}
